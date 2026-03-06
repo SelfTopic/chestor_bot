284135bc22a01f96e405269ea7a88a9fb0e7c493
CheStor <selftopic@gmail.com>
2026-03-06 22:55:34 +0800

    fix ban middleware

--- src/bot/middlewares/ban_middleware.py
+++ src/bot/middlewares/ban_middleware.py
@@ -4,8 +4,10 @@ from typing import Any, Awaitable, Callable, Dict
 
 from aiogram import BaseMiddleware
 from aiogram.types import CallbackQuery, Message, TelegramObject
+from dependency_injector.wiring import Provide
 
-from src.bot.repositories.user import UserRepository
+from src.bot.containers import Container
+from src.bot.services import UserService
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +18,7 @@ class BanMiddleware(BaseMiddleware):
         handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
         event: TelegramObject,
         data: Dict[str, Any],
+        user_service: UserService = Provide[Container.user_service],
     ) -> None:
         user = None
 
@@ -33,14 +36,13 @@ class BanMiddleware(BaseMiddleware):
             await handler(event, data)
             return
 
-        repo = UserRepository(session)
-        db_user = await repo.get(user.id)
+        db_user = await user_service.get(user.id)
 
         if db_user and db_user.is_banned:
             if db_user.banned_until and db_user.banned_until < datetime.now(
                 timezone.utc
             ):
-                await repo.unban(user.id)
+                await user_service.user_repository.unban(user.id)
                 await handler(event, data)
                 return
 
