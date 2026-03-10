dbd63fb122331e41cd99f7cd75928988f2f9f0f7
CheStor <selftopic@gmail.com>
2026-03-10 23:10:09 +0800

    add wordle filter

--- src/bot/filters/__init__.py
+++ src/bot/filters/__init__.py
@@ -1,8 +1,10 @@
 from .rp_command import NewRpCommandOnMedia, RpCommandFilter
 from .text import Text
+from .wordle import WordleGameFilter
 
 __all__ = [
     "Text",
     "RpCommandFilter",
     "NewRpCommandOnMedia",
+    "WordleGameFilter",
 ]
