7d6dc56d1ed6961eb4cccac6d1ef01c91ddb86cd
CheStor <selftopic@gmail.com>
2026-08-10 03:41:51 +0800

    add types init

--- src/bot/types/__init__.py
+++ src/bot/types/__init__.py
@@ -1,7 +1,15 @@
+from .battle import (
+    BattleEvent,
+    BattleFighter,
+    BattleResult,
+    EventType,
+    TimelineEvent,
+    TimelineEventData,
+)
 from .coffee import CoffeeResult
 from .dep import DepColor, DepResult
 from .kagune import KaguneType
-from .media import MediaCollection, MediaDownloadType, MediaSaveRequest
+from .media import MediaCollection, MediaDownloadType, MediaSaveRequest, VideoCutJob
 from .race import Race
 from .register_ghoul import RegisterGhoulType
 from .time_components import Duration, TimeComponents
@@ -18,4 +26,11 @@ __all__ = [
     "DepColor",
     "DepResult",
     "Duration",
+    "BattleFighter",
+    "BattleEvent",
+    "BattleResult",
+    "TimelineEvent",
+    "TimelineEventData",
+    "EventType",
+    "VideoCutJob",
 ]
